55b149e726c58a8231de363aab9dd74950c98bd1
emiliano <echiper.dev@gmail.com>
2024-11-11 08:35:07 +0200

    Update install_lua.sh

--- install_lua.sh
+++ install_lua.sh
@@ -2,8 +2,9 @@
 
 # Install Lua
 curl -R -O ~/lua-5.1.5.tar.gz http://www.lua.org/ftp/lua-5.1.5.tar.gz
-tar -zxf lua-5.3.5.tar.gz
-cd lua-5.1.5
+cd
+tar -zxf lua-5.1.5.tar.gz
+cd ~/lua-5.1.5
 make linux test
 sudo make install
 cd
