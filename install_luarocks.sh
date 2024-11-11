0797771e58697f5c3229ba08c90a8574da3733d7
emiliano <echiper.dev@gmail.com>
2024-11-11 08:33:57 +0200

    Update install_luarocks.sh

--- install_luarocks.sh
+++ install_luarocks.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Install luarocks
-curl -O https://luarocks.github.io/luarocks/releases/luarocks-3.11.1.tar.gz
+curl -O ~/luarocks-3.11.1.tar.gz https://luarocks.github.io/luarocks/releases/luarocks-3.11.1.tar.gz
+cd ~/luarocks-3.11.1.tar.gz
 ./configure --with-lua-include=/usr/local/include
 make
 sudo make install
