d2074df714b00bec8c2c0ec3db215448f795d778
emiliano <echiper.dev@gmail.com>
2024-11-14 21:19:12 +0200

    Update scripts

--- open_tmux.sh
+++ open_tmux.sh
@@ -6,20 +6,24 @@ session_name=$(basename "$PWD")
 # Start a new tmux session with the session name and detach
 tmux new-session -d -s "$session_name"
 
-# Window 1: Navigation
-tmux rename-window -t "$session_name:0" 'Navigation'
+# Window 1: htop in the home directory
+tmux rename-window -t "$session_name:0" 'htop'
+tmux send-keys -t "$session_name:0" 'cd ~ && htop' C-m
 
-# Window 2: Git operations
-tmux new-window -t "$session_name:1" -n 'Git'
-tmux send-keys -t "$session_name:1" 'clear' C-m
+# Window 2: Navigation
+tmux new-window -t "$session_name:1" -n 'Navigation'
 
-# Window 3: Neovim
-tmux new-window -t "$session_name:2" -n 'Neovim'
-tmux send-keys -t "$session_name:2" 'nvim' C-m
+# Window 3: Git operations
+tmux new-window -t "$session_name:2" -n 'Git'
+tmux send-keys -t "$session_name:2" 'clear' C-m
 
-# Window 4: Scripts
-tmux new-window -t "$session_name:3" -n 'Scripts'
-tmux send-keys -t "$session_name:3" 'clear' C-m
+# Window 4: Neovim
+tmux new-window -t "$session_name:3" -n 'Neovim'
+tmux send-keys -t "$session_name:3" 'nvim' C-m
+
+# Window 5: Scripts
+tmux new-window -t "$session_name:4" -n 'Scripts'
+tmux send-keys -t "$session_name:4" 'clear' C-m
 
 # Attach to the session
 tmux attach -t "$session_name"
---
+++ start_openrgb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script to start OpenRGB on system startup without password prompt
+
+cd ~
+sudo /home/emiliano_/OpenRGB_0.9_x86_64_b5f46e3.AppImage
+
